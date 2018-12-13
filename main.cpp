d3fde378df63ebb7fff744beed99d167a9efd50a
2016bits <1835154145@qq.com>
2018-12-13 13:47:17 +0800

    优化后的代码

--- main.cpp
+++ main.cpp
@@ -10,15 +10,17 @@ struct Space {
 };
 class Sudo {
 private:
+	ifstream fin;					//从文件读取输入
+	ofstream fout;					//输出到文件
 	int num_space;					//解数独中空格的个数
 	int array[10][10];				//存放解数独的题目
 	Space space[60];				//存放空格
 	bool ok;						//判断是否已经解决 
 public:
 	void Finality(int num);
-//	void Solution(char* file);
+	void Solution(char* file);
 	void Movesudo(char*, char*, char*, char*, FILE*);
-//	void Tryspace(int);
+	void Tryspace(int);
 };
 void Sudo::Movesudo(char *rule0, char *rule1, char *rule2, char *a, FILE *fout)
 {
@@ -78,83 +80,83 @@ void Sudo::Finality(int num)
 	fputc('\0', fout);
 	fclose(fout);
 }
-//void Sudo::Tryspace(int now)
-//{
-//	//逐个试探空格中的数字
-//	if (ok) {
-//		return;
-//	}
-//	if (now > num_space) {
-//		for (int i = 0; i < 9; ++i) {
-//			for (int j = 0; j < 8; ++j) {
-//				fout << array[i][j] << " ";
-//			}
-//			fout << array[i][8] << endl;
-//		}
-//		return;
-//	}
-//	int row = space[now].row;				//当前空格的所在行
-//	int col = space[now].col;				//当前空格的所在列
-//	int anum = space[now].anum;				//当前空格的所在方格数
-//	for (int i = 1; i <= 9; ++i) {
-//		bool flag = true;					//检测是否符合数独规则
-//		array[row][col] = i;						//试探该空格的值
-//		for (int j = 0; j < 9 && flag; ++j) {
-//			//检测该行是否出现重复的数字
-//			if (array[row][j] == i && j != col) {
-//				flag = false;
-//				break;
-//			}
-//		}
-//		for (int j = 0; j < 9 && flag; ++j) {
-//			//检测该列是否出现重复的数字
-//			if (array[j][col] == i && j != row) {
-//				flag = false;
-//				break;
-//			}
-//		}
-//		for (int j = (anum / 3) * 3; j <= (anum / 3) * 3 + 2 && flag; ++j) {
-//			for (int k = (anum % 3) * 3; k <= (anum % 3) * 3 + 2 && flag; ++k) {
-//				//检测该方格里是否出现重复的数字
-//				if (array[j][k] == i && (j != row || k != col)) {
-//					flag = false;
-//					break;
-//				}
-//			}
-//		}
-//		if (flag) {
-//			//向下一个空格试探
-//			Tryspace(now + 1);
-//		}
-//	}
-//	array[row][col] = 0;
-//}
-//void Sudo::Solution(char* file)
-//{
-//	//求解数独
-//	fin.open(file);
-//	fout.open("sudoku2.txt");
-//	while (!fin.eof()) {
-//		//读取题目
-//		int count = 1;					//空格的编号
-//		for (int i = 0; i < 9; ++i) {
-//			for (int j = 0; j < 9; ++j) {
-//				fin >> array[i][j];
-//				if (array[i][j] == 0) {
-//					space[count].row = i;
-//					space[count].col = j;
-//					space[count++].anum = (i / 3) * 3 + (j / 3);
-//				}
-//			}
-//		}
-//		num_space = count - 1;
-//		ok = false;
-//		Tryspace(1);
-//		fout << endl;
-//	}
-//	fin.close();
-//	fout.close();
-//}
+void Sudo::Tryspace(int now)
+{
+	//逐个试探空格中的数字
+	if (ok) {
+		return;
+	}
+	if (now > num_space) {
+		for (int i = 0; i < 9; ++i) {
+			for (int j = 0; j < 8; ++j) {
+				fout << array[i][j] << " ";
+			}
+			fout << array[i][8] << endl;
+		}
+		return;
+	}
+	int row = space[now].row;				//当前空格的所在行
+	int col = space[now].col;				//当前空格的所在列
+	int anum = space[now].anum;				//当前空格的所在方格数
+	for (int i = 1; i <= 9; ++i) {
+		bool flag = true;					//检测是否符合数独规则
+		array[row][col] = i;						//试探该空格的值
+		for (int j = 0; j < 9 && flag; ++j) {
+			//检测该行是否出现重复的数字
+			if (array[row][j] == i && j != col) {
+				flag = false;
+				break;
+			}
+		}
+		for (int j = 0; j < 9 && flag; ++j) {
+			//检测该列是否出现重复的数字
+			if (array[j][col] == i && j != row) {
+				flag = false;
+				break;
+			}
+		}
+		for (int j = (anum / 3) * 3; j <= (anum / 3) * 3 + 2 && flag; ++j) {
+			for (int k = (anum % 3) * 3; k <= (anum % 3) * 3 + 2 && flag; ++k) {
+				//检测该方格里是否出现重复的数字
+				if (array[j][k] == i && (j != row || k != col)) {
+					flag = false;
+					break;
+				}
+			}
+		}
+		if (flag) {
+			//向下一个空格试探
+			Tryspace(now + 1);
+		}
+	}
+	array[row][col] = 0;
+}
+void Sudo::Solution(char* file)
+{
+	//求解数独
+	fin.open(file);
+	fout.open("sudoku2.txt");
+	while (!fin.eof()) {
+		//读取题目
+		int count = 1;					//空格的编号
+		for (int i = 0; i < 9; ++i) {
+			for (int j = 0; j < 9; ++j) {
+				fin >> array[i][j];
+				if (array[i][j] == 0) {
+					space[count].row = i;
+					space[count].col = j;
+					space[count++].anum = (i / 3) * 3 + (j / 3);
+				}
+			}
+		}
+		num_space = count - 1;
+		ok = false;
+		Tryspace(1);
+		fout << endl;
+	}
+	fin.close();
+	fout.close();
+}
 int main(int argc, char* argv[])
 {//test
 	Sudo sudo;
@@ -172,17 +174,16 @@ int main(int argc, char* argv[])
 		}
 		if (flag) {
 			sudo.Finality(num);
-			printf("over!\n");
 		}
 		else {
 			//输入异常
 			printf("Error!\n");
 		}
 	}
-//	else if (argc == 3 && argv[1][1] == 's') {
-//		//求解数独
-//		sudo.Solution(argv[2]);
-//	}
+	else if (argc == 3 && argv[1][1] == 's') {
+		//求解数独
+		sudo.Solution(argv[2]);
+	}
 	else {
 		//输入异常
 		printf("Error!");

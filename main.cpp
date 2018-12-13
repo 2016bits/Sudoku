60dae37f2292e41662c621c76c6bc1a734681f88
2016bits <1835154145@qq.com>
2018-12-13 13:01:31 +0800

    优化生成终局后的文件

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cstdio>
 #include<cstring>
+#include<cstdlib>
 #include<fstream>
 #include<algorithm>
 using namespace std;
@@ -9,140 +10,151 @@ struct Space {
 };
 class Sudo {
 private:
-	ifstream fin;					//从文件读取输入
-	ofstream fout;					//输出到文件
 	int num_space;					//解数独中空格的个数
 	int array[10][10];				//存放解数独的题目
 	Space space[60];				//存放空格
 	bool ok;						//判断是否已经解决 
 public:
 	void Finality(int num);
-	void Solution(char* file);
-	void Movesudo(int, int, int, int*);
-	void Tryspace(int);
+//	void Solution(char* file);
+	void Movesudo(char*, char*, char*, char*, FILE*);
+//	void Tryspace(int);
 };
-void Sudo::Movesudo(int rule1, int rule2, int rule3, int* a)
+void Sudo::Movesudo(char *rule0, char *rule1, char *rule2, char *a, FILE *fout)
 {
+	//将每个字符型存储的移位规则转换成整形的移位规则 
 	int steps[10];					//记录每行移动的距离
-	steps[0] = 0;
-	steps[1] = rule1 / 10;
-	steps[2] = rule1 % 10;
-	steps[3] = rule2 / 100;
-	steps[4] = (rule2 % 100) / 10;
-	steps[5] = rule2 % 10;
-	steps[6] = rule3 / 100;
-	steps[7] = (rule3 % 100) / 10;
-	steps[8] = rule3 % 10;
+	int num = 0;
+	for (int i = 0; i < 3; ++i) {
+		steps[num++] = rule0[i] - '0';
+	}
+	for (int i = 0; i < 3; ++i) {
+		steps[num++] = rule1[i] - '0';
+	}
+	for (int i = 0; i < 3; ++i) {
+		steps[num++] = rule2[i] - '0';
+	}
+	//开始移位并存储在文件中
+	
 	for (int i = 0; i < 9; ++i) {
 		int step = steps[i];
-		for (int j = 0; j < 8; ++j) {
-			int number = (j + step) % 9;		//记录移动后的下标
-			fout << a[number] << " ";
+		fputc(a[(8+step)%9], fout); 
+		for (int j = 1; j < 17; ++j) {
+			fputc(' ', fout);
+			fputc(a[((16-j)/2 + step) % 9], fout);
+			++j;
 		}
-		fout << a[(8 + step) % 9] << endl;
+		fputc('\n', fout);
 	}
-	fout << endl;
+	fputc('\n', fout);
+	
 }
 void Sudo::Finality(int num)
 {
-	int a[] = { 2, 1, 3, 4, 5, 6, 7, 8, 9 };
-	int rule[8][8] = { {36, 63}, {147, 174, 417, 471, 714, 741}, {258, 285, 528, 582, 825, 852} };
+	char a[15] = { '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	char rule0[10][5] = {"036", "063"};
+	char rule1[10][5] = {"258", "285", "528", "582", "825", "852"};
+	char rule2[10][5] = {"147", "174", "417", "471", "714", "741"};
 	//生成终局
-	fout.open("sudoku.txt");
+	FILE *fout = fopen("sudoku.txt", "w");
 	int count = 0;					//记录终局个数
+	bool flag = true;
 	do {
+		a[8] = '3';					//插入学号 :（2+0）% 9 + 1 = 3 
 		//平移的2*6*6种方式
-		for (int i = 0; i < 2; ++i) {
-			for (int j = 0; j < 6; ++j) {
-				for (int k = 0; k < 6; ++k) {
-					Movesudo(rule[0][i], rule[1][j], rule[2][k], a);
+		for (int i = 0; i < 2 && flag; ++i) {
+			for (int j = 0; j < 6 && flag; ++j) {
+				for (int k = 0; k < 6 && flag; ++k) {
+					Movesudo(rule0[i], rule1[j], rule2[k], a, fout);
 					++count;
+					if (count >= num) {
+						flag = false;
+						break;
+					}
 				}
 			}
-		}
-		if (count > num) {
-			break;
 		}
 	} while (next_permutation(a + 1, a + 8));			//使用STL全排列函数
-	fout.close();
+	fputc('\0', fout);
+	fclose(fout);
 }
-void Sudo::Tryspace(int now)
-{
-	//逐个试探空格中的数字
-	if (ok) {
-		return;
-	}
-	if (now > num_space) {
-		for (int i = 0; i < 9; ++i) {
-			for (int j = 0; j < 8; ++j) {
-				fout << array[i][j] << " ";
-			}
-			fout << array[i][8] << endl;
-		}
-		return;
-	}
-	int row = space[now].row;				//当前空格的所在行
-	int col = space[now].col;				//当前空格的所在列
-	int anum = space[now].anum;				//当前空格的所在方格数
-	for (int i = 1; i <= 9; ++i) {
-		bool flag = true;					//检测是否符合数独规则
-		array[row][col] = i;						//试探该空格的值
-		for (int j = 0; j < 9 && flag; ++j) {
-			//检测该行是否出现重复的数字
-			if (array[row][j] == i && j != col) {
-				flag = false;
-				break;
-			}
-		}
-		for (int j = 0; j < 9 && flag; ++j) {
-			//检测该列是否出现重复的数字
-			if (array[j][col] == i && j != row) {
-				flag = false;
-				break;
-			}
-		}
-		for (int j = (anum / 3) * 3; j <= (anum / 3) * 3 + 2 && flag; ++j) {
-			for (int k = (anum % 3) * 3; k <= (anum % 3) * 3 + 2 && flag; ++k) {
-				//检测该方格里是否出现重复的数字
-				if (array[j][k] == i && (j != row || k != col)) {
-					flag = false;
-					break;
-				}
-			}
-		}
-		if (flag) {
-			//向下一个空格试探
-			Tryspace(now + 1);
-		}
-	}
-	array[row][col] = 0;
-}
-void Sudo::Solution(char* file)
-{
-	//求解数独
-	fin.open(file);
-	fout.open("sudoku2.txt");
-	while (!fin.eof()) {
-		//读取题目
-		int count = 1;					//空格的编号
-		for (int i = 0; i < 9; ++i) {
-			for (int j = 0; j < 9; ++j) {
-				fin >> array[i][j];
-				if (array[i][j] == 0) {
-					space[count].row = i;
-					space[count].col = j;
-					space[count++].anum = (i / 3) * 3 + (j / 3);
-				}
-			}
-		}
-		num_space = count - 1;
-		ok = false;
-		Tryspace(1);
-		fout << endl;
-	}
-	fin.close();
-	fout.close();
-}
+//void Sudo::Tryspace(int now)
+//{
+//	//逐个试探空格中的数字
+//	if (ok) {
+//		return;
+//	}
+//	if (now > num_space) {
+//		for (int i = 0; i < 9; ++i) {
+//			for (int j = 0; j < 8; ++j) {
+//				fout << array[i][j] << " ";
+//			}
+//			fout << array[i][8] << endl;
+//		}
+//		return;
+//	}
+//	int row = space[now].row;				//当前空格的所在行
+//	int col = space[now].col;				//当前空格的所在列
+//	int anum = space[now].anum;				//当前空格的所在方格数
+//	for (int i = 1; i <= 9; ++i) {
+//		bool flag = true;					//检测是否符合数独规则
+//		array[row][col] = i;						//试探该空格的值
+//		for (int j = 0; j < 9 && flag; ++j) {
+//			//检测该行是否出现重复的数字
+//			if (array[row][j] == i && j != col) {
+//				flag = false;
+//				break;
+//			}
+//		}
+//		for (int j = 0; j < 9 && flag; ++j) {
+//			//检测该列是否出现重复的数字
+//			if (array[j][col] == i && j != row) {
+//				flag = false;
+//				break;
+//			}
+//		}
+//		for (int j = (anum / 3) * 3; j <= (anum / 3) * 3 + 2 && flag; ++j) {
+//			for (int k = (anum % 3) * 3; k <= (anum % 3) * 3 + 2 && flag; ++k) {
+//				//检测该方格里是否出现重复的数字
+//				if (array[j][k] == i && (j != row || k != col)) {
+//					flag = false;
+//					break;
+//				}
+//			}
+//		}
+//		if (flag) {
+//			//向下一个空格试探
+//			Tryspace(now + 1);
+//		}
+//	}
+//	array[row][col] = 0;
+//}
+//void Sudo::Solution(char* file)
+//{
+//	//求解数独
+//	fin.open(file);
+//	fout.open("sudoku2.txt");
+//	while (!fin.eof()) {
+//		//读取题目
+//		int count = 1;					//空格的编号
+//		for (int i = 0; i < 9; ++i) {
+//			for (int j = 0; j < 9; ++j) {
+//				fin >> array[i][j];
+//				if (array[i][j] == 0) {
+//					space[count].row = i;
+//					space[count].col = j;
+//					space[count++].anum = (i / 3) * 3 + (j / 3);
+//				}
+//			}
+//		}
+//		num_space = count - 1;
+//		ok = false;
+//		Tryspace(1);
+//		fout << endl;
+//	}
+//	fin.close();
+//	fout.close();
+//}
 int main(int argc, char* argv[])
 {//test
 	Sudo sudo;
@@ -160,16 +172,17 @@ int main(int argc, char* argv[])
 		}
 		if (flag) {
 			sudo.Finality(num);
+			printf("over!\n");
 		}
 		else {
 			//输入异常
 			printf("Error!\n");
 		}
 	}
-	else if (argc == 3 && argv[1][1] == 's') {
-		//求解数独
-		sudo.Solution(argv[2]);
-	}
+//	else if (argc == 3 && argv[1][1] == 's') {
+//		//求解数独
+//		sudo.Solution(argv[2]);
+//	}
 	else {
 		//输入异常
 		printf("Error!");
